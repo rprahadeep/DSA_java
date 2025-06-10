fe66a0dbc958c3d5880defce147898a16ded813e
rprahadeep <rprahadeep@gmail.com>
2025-06-10 18:53:35 +0530

    AVL

--- Trees/src/Main.java
+++ Trees/src/Main.java
@@ -1,12 +1,11 @@
 import java.util.*;
 public class Main{
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        BST tree = new BST();
-        int[] nums = {5,2,7,1,4,6,9,8,3,10};
-        tree.populate(nums);
+        AVL tree = new AVL();
+        for(int i=0;i<1000;i++){
+            tree.insert(i);
+        }
 
-        tree.display();
-
+        System.out.println(tree.height());
     }
 }
