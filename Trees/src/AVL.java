db8f41cff232935591f308c735d4cb761c45a468
rprahadeep <rprahadeep@gmail.com>
2025-06-10 18:57:27 +0530

    AVL update

--- Trees/src/AVL.java
+++ Trees/src/AVL.java
@@ -103,8 +103,8 @@ class AVL {
         p.left = c;
         c.right = t;
 
-        p.height = Math.max(height(p.left), height(p.right) + 1);
         c.height = Math.max(height(c.left), height(c.right) + 1);
+        p.height = Math.max(height(p.left), height(p.right) + 1);
 
         return p;
     }
