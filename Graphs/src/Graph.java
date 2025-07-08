c699b1e4d2b8c1816b8bb930e67e09765bafa4bc
rprahadeep <rprahadeep@gmail.com>
2025-07-08 16:00:56 +0530

    adjacency list

---
+++ Graphs/src/Graph.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+//using adjacency list
+public class Graph {
+    private int vertices;
+    ArrayList<Edge>[] graph;
+
+    public class Edge{
+        int source;
+        int destination;
+        int weight;
+
+        Edge(int s, int w, int d) {
+            this.source = s;
+            this.weight = w;
+            this.destination = d;
+        }
+    }
+
+    public void addEdge(int source, int weight, int destination){
+        graph[source].add(new Edge(source, weight, destination));
+    }
+
+    public void display(){
+        for(int i=0;i<vertices;i++){
+            for(Edge e:graph[i]){
+                System.out.print(e.source + "->" + e.destination + " - Weight = " + e.weight + ", ");
+            }
+            System.out.println();
+        }
+    }
+
+    Graph(int v){
+        this.vertices = v;
+        graph = new ArrayList[v];
+        for (int i = 0; i < v; i++) {
+            graph[i] = new ArrayList<>();
+        }
+    }
+}
--- Graphs/src/Main.java
+++ Graphs/src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class Main{
+    public static void main(String[] args){
+        Graph graph =  new Graph(4);
+        graph.addEdge(0,2,2);
+        graph.addEdge(1,10,2);
+        graph.addEdge(1,0,3);
+        graph.addEdge(2,2,0);
+        graph.addEdge(2,10,1);
+        graph.addEdge(2,-1,3);
+        graph.addEdge(3,0,1);
+        graph.addEdge(3,-1,2);
+
+        graph.display();
     }
 }
